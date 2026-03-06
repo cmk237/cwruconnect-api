644cff28f2b269d8c320264bed8605a3868f651f
cmk237 <cmk237@case.edu>
2026-03-06 01:45:24 -0500

    _

--- app.py
+++ app.py
@@ -90,13 +90,12 @@ def get_my_connections():
     cur = conn.cursor(cursor_factory=RealDictCursor)
     try:
         sql = """
-            select users.userID, users.name, users.minibio
-from users
-where users.userID in (SELECT c.userID2
-FROM Connections c
-WHERE c.userID1 = %s;
+            SELECT u.userID, u.name, u.minibio
+            FROM Users u
+            JOIN Connections c ON (u.userID = c.userID1 OR u.userID = c.userID2)
+            WHERE (c.userID1 = %s OR c.userID2 = %s) AND u.userID != %s;
         """
-        cur.execute(sql, (user_id))
+        cur.execute(sql, (user_id, user_id, user_id))
         results = cur.fetchall()
         return jsonify(results), 200
     except Exception as e:
